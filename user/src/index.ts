763f1fb6c32fec024c60cb28670171e9904c2869
thasup <thanachonfirst@hotmail.com>
2022-02-12 13:37:02 +0700

    setup ingress service

--- user/src/index.ts
+++ user/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`User server: Listening on port ${port}`);
