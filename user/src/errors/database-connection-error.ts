8d1b9b019258c6e17ebaa89ba9bacb21c758aec2
thasup <thanachonfirst@hotmail.com>
2022-02-12 19:45:14 +0700

    Refactor error handling

--- user/src/errors/database-connection-error.ts
+++ user/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = "Error connecting to database";
 
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- user/src/middlewares/error-handler.ts
+++ user/src/middlewares/error-handler.ts
@@ -9,16 +9,13 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log("handling this error as a request validation error");
-    const formattedErrors = err.errors.map((error) => {
-      return { message: error.msg, field: error.param };
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    // handling this error as a request validation error
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log("handling this error as a database connection error");
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    // handling this error as a database connection error
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
