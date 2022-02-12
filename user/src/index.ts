a27648fc9782c3a6c5c3748cc6276608f3a1b262
thasup <thanachonfirst@hotmail.com>
2022-02-12 16:43:21 +0700

    deploy on GKE

--- user/src/index.ts
+++ user/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+  res.send("Hi there!!!!");
 });
 
 const port = 3000;
