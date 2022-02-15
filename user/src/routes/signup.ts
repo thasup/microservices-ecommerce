f6f20726cab17eca2467850d7b499fad0c3d46ed
thasup <thanachonfirst@hotmail.com>
2022-02-15 23:56:53 +0700

    Add validateRequest middleware

--- user/src/routes/signup.ts
+++ user/src/routes/signup.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
 import { User } from "../models/user";
-import { RequestValidationError } from "../errors/RequestValidationError";
 import { BadRequestError } from "../errors/BadRequestError";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -17,13 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
