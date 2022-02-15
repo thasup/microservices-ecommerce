5d1d3d57b8fe5d794ad55ce7826743d960c45537
thasup <thanachonfirst@hotmail.com>
2022-02-16 00:56:51 +0700

    Add current-user, require-auth middleware

---
+++ user/src/errors/NotAuthorizedError.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./CustomError";
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super("Not authorized");
+
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: "Not authorized" }];
+  }
+}
