27df5dc40e3c88aef864766ea43dd003d0645557
thasup <thanachonfirst@hotmail.com>
2022-02-16 12:50:09 +0700

    Set up client with Next.js

---
+++ client/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <h1>Hello Next!</h1>;
+};
+
+export default index;
--- user/src/test/setup.ts
+++ user/src/test/setup.ts
@@ -27,8 +27,8 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  await mongo.stop();
   await mongoose.connection.close();
+  await mongo.stop();
 });
 
 global.signin = async () => {
