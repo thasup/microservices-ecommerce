9be35748558781675addbb92db5fb039d17c772e
thasup <thanachonfirst@hotmail.com>
2022-02-25 00:29:28 +0700

    Add create-order route

--- order/src/routes/create-order.ts
+++ order/src/routes/create-order.ts
@@ -1,9 +1,71 @@
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@thasup-dev/common";
+import { Product } from "../models/product";
+import { Order } from "../models/order";
+import { natsWrapper } from "../NatsWrapper";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+const EXPIRATION_WINDOW_SECONDS = 30 * 60;
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("productId").not().isEmpty().withMessage("ProductId must be provided"),
+    body("productId").isMongoId().withMessage("Invalid MongoDB ObjectId"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { productId } = req.body;
+
+    // Find the product the user is trying to order in the database
+    const product = await Product.findById(productId);
+    if (!product) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this product is not already reserved
+    // Check if the product has only 1 set. If true then run query
+    // to look at all orders. Find an order where the product
+    // is the product we just found *AND* thr orders status is *NOT* cancelled.
+    // If we find an order from that means the product *IS* reserved
+    if (product.countInStock === 1) {
+      const existingOrder = await Order.findOne({
+        product: product,
+        status: {
+          $in: [OrderStatus.Created, OrderStatus.Pending, OrderStatus.Complete],
+        },
+      });
+      if (existingOrder) {
+        throw new BadRequestError("Product is already reserved");
+      }
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      product,
+    });
+    await order.save();
+
+    // Publish an event saying that an order was created
+
+    res.status(201).send(order);
+  }
+);
 
 export { router as createOrderRouter };
