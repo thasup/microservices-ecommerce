fec916c830f11a127ba53a51cee840f961a121e1
thasup <thanachonfirst@hotmail.com>
2022-02-28 08:33:15 +0700

    Setup new payment service

---
+++ payment/src/test/setup.ts
@@ -0,0 +1,85 @@
+import jwt from "jsonwebtoken";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+declare global {
+  var signin: () => string[];
+  var adminSignin: () => string[];
+}
+
+jest.mock("../NatsWrapper");
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "missiontothemoon";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoURI = mongo.getUri();
+
+  await mongoose.connect(mongoURI);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
+
+global.signin = () => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+    password: "password",
+    isAdmin: false,
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
+
+global.adminSignin = () => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+    password: "password",
+    isAdmin: true,
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
