5ade9d31427dd412762a51066b1e943eba16d76a
thasup <thanachonfirst@hotmail.com>
2022-03-12 14:51:47 +0700

    Update hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.aurapan.com",
       headers: req.headers,
       withCredentials: true,
     });
