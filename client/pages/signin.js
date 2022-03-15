819a223f945af785f47ae27ec0298e3f99143ecd
thasup <thanachonfirst@hotmail.com>
2022-03-15 17:48:11 +0700

    Add: input field in sign-up page

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Router from "next/router";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 import useRequest from "../hooks/use-request";
-import { Button, Col, Form, Row } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 
 const signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
 
   const { doRequest, errors } = useRequest({
     url: "/api/users/signin",
@@ -16,51 +18,59 @@ const signin = () => {
       email,
       password,
     },
-    onSuccess: () => Router.push("/"),
+    onSuccess: () => {
+      Router.push("/");
+      setLoading(false);
+    },
   });
 
   const submitHandler = async (event) => {
     event.preventDefault();
 
-    const response = await doRequest();
-    console.log(response);
+    setLoading(true);
+    doRequest();
   };
 
-  return (
-    <Row>
-      <Col sm={8}>
-        <FormContainer>
-          <h1>Sign In</h1>
-          <Form className="mt-3" onSubmit={submitHandler}>
-            <Form.Group controlId="email" className="my-3">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
+  return loading ? (
+    <div className="d-flex justify-content-center align-items-center px-0">
+      <Loader />
+    </div>
+  ) : (
+    <Container className="app-container">
+      <Row>
+        <Col>
+          <FormContainer>
+            <h1>Sign In</h1>
+            <Form className="mt-3" onSubmit={submitHandler}>
+              <Form.Group controlId="email" className="my-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-            <Form.Group controlId="password" className="my-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
+              <Form.Group controlId="password" className="my-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-            {errors}
-            <Button className="mt-3" type="submit" variant="dark">
-              Sign In
-            </Button>
-          </Form>
-        </FormContainer>
-      </Col>
-      <Col sm={4}></Col>
-    </Row>
+              {errors}
+              <Button className="mt-3" type="submit" variant="dark">
+                Sign In
+              </Button>
+            </Form>
+          </FormContainer>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,66 +1,117 @@
 import React, { useState } from "react";
 import Router from "next/router";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 import useRequest from "../hooks/use-request";
-import styles from "../styles/signup.module.css";
-import { Button, Col, Form, Row } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
 
 const signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState(null);
+  const [age, setAge] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
   const { doRequest, errors } = useRequest({
     url: "/api/users/signup",
     method: "post",
     body: {
       email,
       password,
+      name,
+      gender,
+      age,
     },
-    onSuccess: () => Router.push("/"),
+    onSuccess: () => {
+      Router.push("/");
+      setLoading(false);
+    },
   });
 
   const submitHandler = async (event) => {
     event.preventDefault();
+    setLoading(true);
 
-    const response = await doRequest();
-    console.log(response);
+    doRequest();
   };
 
-  return (
-    <Row>
-      <Col sm={8}>
-        <FormContainer>
-          <h1>Sign Up</h1>
-          <Form className="mt-3" onSubmit={submitHandler}>
-            <Form.Group controlId="email" className="my-3">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
+  return loading ? (
+    <div className="d-flex justify-content-center align-items-center px-0">
+      <Loader />
+    </div>
+  ) : (
+    <Container className="app-container">
+      <Row>
+        <Col>
+          <FormContainer>
+            <h1>Sign Up</h1>
+            <Form className="mt-3" onSubmit={submitHandler}>
+              <Form.Group controlId="email" className="my-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-            <Form.Group controlId="password" className="my-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
+              <Form.Group controlId="password" className="my-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-            {errors}
-            <Button className="mt-3" type="submit" variant="dark">
-              Sign Up
-            </Button>
-          </Form>
-        </FormContainer>
-      </Col>
-      <Col sm={4}></Col>
-    </Row>
+              <Form.Group controlId="name" className="my-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+
+              <Form.Group controlId="gender" className="my-3">
+                <Form.Label>Gender</Form.Label>
+                <Form.Control
+                  as="select"
+                  className="form-select"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group controlId="age" className="my-3">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Enter age"
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+
+              {errors}
+              <Button className="mt-3" type="submit" variant="dark">
+                Sign Up
+              </Button>
+            </Form>
+          </FormContainer>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
