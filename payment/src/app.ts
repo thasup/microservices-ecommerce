fe24036a46cf9422e12a5c5b16be51a00f63c081
thasup <thanachonfirst@hotmail.com>
2022-03-24 23:51:25 +0700

    Edit: config files for production mode

--- payment/src/app.ts
+++ payment/src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(
   cookieSession({
     signed: false,
-    secure: false,
+    secure: process.env.NODE_ENV === "production",
   })
 );
 app.use(currentUser);
--- product/src/app.ts
+++ product/src/app.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cookieSession({
     signed: false,
-    secure: false,
+    secure: process.env.NODE_ENV === "production",
   })
 );
 app.use(currentUser);
