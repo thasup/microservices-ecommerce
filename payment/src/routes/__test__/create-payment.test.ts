95ef1a9b7a5df1baa3d37f25fc10e88d040b34c2
thasup <thanachonfirst@hotmail.com>
2022-04-03 02:17:35 +0700

    Refactor: payment test scripts

--- payment/src/routes/__test__/create-payment.test.ts
+++ payment/src/routes/__test__/create-payment.test.ts
@@ -9,110 +9,8 @@ import { stripe } from "../../stripe";
 import { Payment } from "../../models/payment";
 
 // jest.mock("../stripe");
-it("returns a 404 when purchasing an order that does not exist", async () => {
-  await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin())
-    .send({
-      token: "asldkfj",
-      orderId: new mongoose.Types.ObjectId().toHexString(),
-    })
-    .expect(404);
-});
 
-it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
-  // Create and save a product
-  const product = Product.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: "Sample Dress",
-    price: 1990,
-    userId: new mongoose.Types.ObjectId().toHexString(),
-    image: "./asset/sample.jpg",
-    colors: "White,Black",
-    sizes: "S,M,L",
-    countInStock: 1,
-    numReviews: 0,
-    rating: 0,
-    isReserved: false,
-  });
-  await product.save();
-
-  const itemsPrice = parseFloat(product.price.toFixed(2));
-  const taxPrice = parseFloat((product.price * 0.07).toFixed(2));
-
-  // Create and save the order
-  const order = Order.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    status: OrderStatus.Created,
-    userId: new mongoose.Types.ObjectId().toHexString(),
-    version: 0,
-    paymentMethod: "stripe",
-    itemsPrice,
-    shippingPrice: 0,
-    taxPrice,
-    totalPrice: itemsPrice + taxPrice,
-  });
-
-  await order.save();
-
-  await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin())
-    .send({
-      token: "asldkfj",
-      orderId: order.id,
-    })
-    .expect(401);
-});
-
-it("returns a 400 when purchasing a cancelled order", async () => {
-  const userId = new mongoose.Types.ObjectId().toHexString();
-
-  // Create and save a product
-  const product = Product.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: "Sample Dress",
-    price: 1990,
-    userId: new mongoose.Types.ObjectId().toHexString(),
-    image: "./asset/sample.jpg",
-    colors: "White,Black",
-    sizes: "S,M,L",
-    countInStock: 1,
-    numReviews: 0,
-    rating: 0,
-    isReserved: false,
-  });
-  await product.save();
-
-  const itemsPrice = parseFloat(product.price.toFixed(2));
-  const taxPrice = parseFloat((product.price * 0.07).toFixed(2));
-
-  // Create and save the order
-  const order = Order.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    status: OrderStatus.Cancelled,
-    userId: userId,
-    version: 0,
-    paymentMethod: "stripe",
-    itemsPrice,
-    shippingPrice: 0,
-    taxPrice,
-    totalPrice: itemsPrice + taxPrice,
-  });
-  await order.save();
-
-  await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin(userId))
-    .send({
-      token: "asdlkfj",
-      orderId: order.id,
-    })
-    .expect(400);
-});
-
-it("returns a 201 with valid inputs", async () => {
-  const userId = new mongoose.Types.ObjectId().toHexString();
+const setup = async (userId?: string) => {
   const price = Math.floor(Math.random() * 100000);
 
   // Create and save a product
@@ -138,7 +36,7 @@ it("returns a 201 with valid inputs", async () => {
   const order = Order.build({
     id: new mongoose.Types.ObjectId().toHexString(),
     status: OrderStatus.Created,
-    userId: userId,
+    userId: userId || new mongoose.Types.ObjectId().toHexString(),
     version: 0,
     paymentMethod: "stripe",
     itemsPrice: itemsPrice,
@@ -147,7 +45,56 @@ it("returns a 201 with valid inputs", async () => {
     totalPrice: itemsPrice + taxPrice,
   });
   await order.save();
-  console.log(order);
+
+  return order;
+};
+
+it("returns a 404 when purchasing an order that does not exist", async () => {
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "randomToken",
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
+  // Create and save a product
+  const order = await setup();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "randomToken",
+      orderId: order.id,
+    })
+    .expect(401);
+});
+
+it("returns a 400 when purchasing a cancelled order", async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  // Create and save a product
+  const order = await setup(userId);
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(userId))
+    .send({
+      token: "randomToken",
+      orderId: order.id,
+    })
+    .expect(400);
+});
+
+it("returns a 201 with valid inputs", async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  // Create product and order
+  const order = await setup(userId);
 
   await request(app)
     .post("/api/payments")
@@ -176,5 +123,6 @@ it("returns a 201 with valid inputs", async () => {
     orderId: order.id,
     stripeId: stripeCharge!.id,
   });
+
   expect(payment).not.toBeNull();
 });
