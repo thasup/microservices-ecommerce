874cc28454d1b421f82e42fe3cb4050348f5a43a
thasup <thanachonfirst@hotmail.com>
2022-04-05 22:24:42 +0700

    Edit: change banner image and styles
    also Protect unauthorized access to sign in & sign up page

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -8,12 +8,13 @@ import Image from "next/image";
 import useRequest from "../hooks/use-request";
 import Loader from "../components/Loader";
 
-const signin = () => {
+const signin = ({ currentUser }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const [loading, setLoading] = useState(false);
   const [showErrors, setShowErrors] = useState(false);
+  const [isReady, setIsReady] = useState(false);
 
   const { doRequest, errors } = useRequest({
     url: "/api/users/signin",
@@ -29,6 +30,13 @@ const signin = () => {
   });
 
   useEffect(() => {
+    // Protect unauthorized access
+    if (currentUser) {
+      return Router.push("/");
+    } else {
+      setIsReady(true);
+    }
+
     if (errors) {
       setLoading(false);
       setShowErrors(true);
@@ -47,75 +55,78 @@ const signin = () => {
   };
 
   return (
-    <>
-      <Head>
-        <title>Sign In | Aurapan</title>
-      </Head>
-      {loading ? (
-        <div
-          className="d-flex justify-content-center align-items-center px-0"
-          style={{ marginTop: "80px" }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <Container className="app-container register-box">
-          <Row>
-            <Link href={`/signup`} passHref>
-              <Col className="banner-img">
-                <Image
-                  loader={myLoader}
-                  src="sign_up_banner.png"
-                  layout="fill"
-                  objectFit="cover"
-                  priority="true"
-                  alt="sign up banner"
-                />
-              </Col>
-            </Link>
-
-            <Col>
-              <h1>Sign In</h1>
-              <Form className="mt-3" onSubmit={submitHandler}>
-                <Form.Group controlId="email" className="my-3">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="password" className="my-3">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-
-                {showErrors ? errors : null}
-                <Button className="mt-3" type="submit" variant="dark">
-                  Sign In
-                </Button>
-              </Form>
-
-              <Row className="py-3">
-                <Col>
-                  New here ?{" "}
-                  <Link href="/signup">
-                    <a>Create an Account</a>
-                  </Link>
+    isReady && (
+      <>
+        <Head>
+          <title>Sign In | Aurapan</title>
+        </Head>
+        {loading ? (
+          <div
+            className="d-flex justify-content-center align-items-center px-0"
+            style={{ marginTop: "80px" }}
+          >
+            <Loader />
+          </div>
+        ) : (
+          <Container className="app-container register-box">
+            <Row>
+              <Link href={`/signup`} passHref>
+                <Col className="banner-img">
+                  <Image
+                    loader={myLoader}
+                    src="sign_up_banner_1.png"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="left center"
+                    priority="true"
+                    alt="sign up banner"
+                  />
                 </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      )}
-    </>
+              </Link>
+
+              <Col>
+                <h1>Sign In</h1>
+                <Form className="mt-3" onSubmit={submitHandler}>
+                  <Form.Group controlId="email" className="my-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group controlId="password" className="my-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Enter password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
+
+                  {showErrors ? errors : null}
+                  <Button className="mt-3" type="submit" variant="dark">
+                    Sign In
+                  </Button>
+                </Form>
+
+                <Row className="py-3">
+                  <Col className="px-0">
+                    New here?{" "}
+                    <Link href="/signup">
+                      <a>Create an Account</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        )}
+      </>
+    )
   );
 };
 
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Router from "next/router";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 import useRequest from "../hooks/use-request";
 import Loader from "../components/Loader";
 
-const signup = () => {
+const signup = ({ currentUser }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
@@ -15,6 +16,8 @@ const signup = () => {
   const [age, setAge] = useState(undefined);
 
   const [loading, setLoading] = useState(false);
+  const [showErrors, setShowErrors] = useState(false);
+  const [isReady, setIsReady] = useState(false);
 
   const { doRequest, errors } = useRequest({
     url: "/api/users/signup",
@@ -32,6 +35,20 @@ const signup = () => {
     },
   });
 
+  useEffect(() => {
+    // Protect unauthorized access
+    if (currentUser) {
+      return Router.push("/");
+    } else {
+      setIsReady(true);
+    }
+
+    if (errors) {
+      setLoading(false);
+      setShowErrors(true);
+    }
+  }, [errors]);
+
   const submitHandler = async (event) => {
     event.preventDefault();
     setLoading(true);
@@ -44,98 +61,109 @@ const signup = () => {
   };
 
   return (
-    <>
-      <Head>
-        <title>Sign Up | Aurapan</title>
-      </Head>
-      {loading ? (
-        <div
-          className="d-flex justify-content-center align-items-center px-0"
-          style={{ marginTop: "80px" }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <Container className="app-container register-box">
-          <Row>
-            <Col className="banner-img">
-              <Image
-                loader={myLoader}
-                src="sign_up_banner.png"
-                layout="fill"
-                objectFit="cover"
-                priority="true"
-                alt="sign up banner"
-              />
-            </Col>
+    isReady && (
+      <>
+        <Head>
+          <title>Sign Up | Aurapan</title>
+        </Head>
+        {loading ? (
+          <div
+            className="d-flex justify-content-center align-items-center px-0"
+            style={{ marginTop: "80px" }}
+          >
+            <Loader />
+          </div>
+        ) : (
+          <Container className="app-container register-box">
+            <Row>
+              <Link href={`/signin`} passHref>
+                <Col className="banner-img">
+                  <Image
+                    loader={myLoader}
+                    src="sign_in_banner_1.png"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="left center"
+                    priority="true"
+                    alt="sign up banner"
+                  />
+                </Col>
+              </Link>
 
-            <Col>
-              <h1>Sign Up</h1>
-              <Form className="mt-3" onSubmit={submitHandler}>
-                <Form.Group controlId="email" className="my-3">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+              <Col>
+                <h1>Sign Up</h1>
+                <Form className="mt-3" onSubmit={submitHandler}>
+                  <Form.Group controlId="email" className="my-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
 
-                <Form.Group controlId="password" className="my-3">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+                  <Form.Group controlId="password" className="my-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Enter password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
 
-                <Form.Group controlId="name" className="my-3">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+                  <Form.Group controlId="name" className="my-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
 
-                <Form.Group controlId="gender" className="my-3">
-                  <Form.Label>Gender</Form.Label>
-                  <Form.Control
-                    as="select"
-                    className="form-select"
-                    value={gender}
-                    onChange={(e) => setGender(e.target.value)}
-                  >
-                    <option value="">Select Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                  </Form.Control>
-                </Form.Group>
+                  <Row className="my-3">
+                    <Col sm={6} className="ps-0 pe-3">
+                      <Form.Group controlId="gender">
+                        <Form.Label>Gender</Form.Label>
+                        <Form.Control
+                          as="select"
+                          className="form-select"
+                          value={gender}
+                          onChange={(e) => setGender(e.target.value)}
+                        >
+                          <option value="">Select Gender</option>
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
+                        </Form.Control>
+                      </Form.Group>
+                    </Col>
 
-                <Form.Group controlId="age" className="my-3">
-                  <Form.Label>Age</Form.Label>
-                  <Form.Control
-                    type="number"
-                    placeholder="Enter age"
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+                    <Col sm={6} className="ps-3 pe-0">
+                      <Form.Group controlId="age">
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control
+                          type="number"
+                          placeholder="Enter age"
+                          value={age}
+                          onChange={(e) => setAge(e.target.value)}
+                        ></Form.Control>
+                      </Form.Group>
+                    </Col>
+                  </Row>
 
-                {errors}
-                <Button className="mt-3" type="submit" variant="dark">
-                  Sign Up
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      )}
-    </>
+                  {showErrors ? errors : null}
+                  <Button className="mt-3" type="submit" variant="dark">
+                    Sign Up
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        )}
+      </>
+    )
   );
 };
 
