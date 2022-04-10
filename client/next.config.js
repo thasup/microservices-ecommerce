e968692910b43ab81d0a9506e07053eb208cb22a
thasup <thanachonfirst@hotmail.com>
2022-04-10 23:51:31 +0700

    Optimization: add cache-control header for static image optimization

--- client/next.config.js
+++ client/next.config.js
@@ -4,4 +4,28 @@ module.exports = {
     loader: "custom",
     minimumCacheTTL: 86400,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*).jpg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=86400, s-maxage=86400, stale-while-revalidate=172800",
+          },
+        ],
+      },
+      {
+        source: "/(.*).png",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=86400, s-maxage=86400, stale-while-revalidate=172800",
+          },
+        ],
+      },
+    ];
+  },
 };
