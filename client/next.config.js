4594f0955d8b2fbe1694f116c09e91f816387bc9
thasup <thanachonfirst@hotmail.com>
2022-04-12 11:08:36 +0700

    Fix: box-shadow stack on product image at product page

--- client/next.config.js
+++ client/next.config.js
@@ -1,28 +1,35 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    loader: "custom",
+    // loader: "custom",
+    domains: ["www.dropbox.com"],
     minimumCacheTTL: 86400,
   },
   async headers() {
     return [
       {
-        source: "/(.*).jpg",
+        source: "/(.*).(jpg|png)",
         headers: [
           {
             key: "Cache-Control",
             value:
-              "public, max-age=86400, s-maxage=86400, stale-while-revalidate=172800",
+              "public, max-age=31536000, s-maxage=31536000, must-revalidate",
           },
         ],
       },
       {
-        source: "/(.*).png",
+        source: "/_next/image(.*)",
+        // has: [
+        //   {
+        //     type: "content-type",
+        //     key: "application/octet-stream",
+        //   },
+        // ],
         headers: [
           {
             key: "Cache-Control",
             value:
-              "public, max-age=86400, s-maxage=86400, stale-while-revalidate=172800",
+              "public, max-age=86401, s-maxage=86401, stale-while-revalidate=172801",
           },
         ],
       },
