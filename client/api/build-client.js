468e73df5ce8c8439bd3bb92957a82c037726b03
thasup <thanachonfirst@hotmail.com>
2022-05-01 22:44:59 +0700

    Edit yaml files to support new GCP cluster

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: "https://www.aurapan.com",
+      baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
       withCredentials: true,
     });
