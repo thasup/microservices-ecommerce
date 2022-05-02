04f298d459733b35635713ec98b71abb84aa88a8
thasup <thanachonfirst@hotmail.com>
2022-05-03 00:12:33 +0700

    Fix: useLayoutEffect issue with ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,13 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
 
 const Home = ({ products, currentUser }) => {
   const [onMobile, setOnMobile] = useState(false);
+  const [showChild, setShowChild] = useState(false);
   const [screenWidth, setScreenWidth] = useState(0);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     // Check current window width to determine screen type
     function updateSize() {
       setScreenWidth(window.innerWidth);
@@ -24,16 +25,22 @@ const Home = ({ products, currentUser }) => {
     return () => window.removeEventListener("resize", updateSize);
   }, [screenWidth]);
 
+  // Wait until after client-side hydration to show
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
   return (
     <>
       <Row className="mx-0">
         {products.map((item) => (
           <Col key={item.id} xs={6} md={4} xl={3} className="p-0">
+            {showChild ? (
             <Product
               onMobile={onMobile}
               product={item}
               currentUser={currentUser}
-            />
+            />) : null}
           </Col>
         ))}
       </Row>
