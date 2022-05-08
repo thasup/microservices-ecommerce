4172c24961d9b3cc9ef55fddab8b2bf3e9ceacf9
thasup <thanachonfirst@hotmail.com>
2022-05-09 01:29:02 +0700

    Add: add eslint and config files

--- client/next.config.js
+++ client/next.config.js
@@ -1,37 +1,42 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["www.dropbox.com"],
-    minimumCacheTTL: 86400,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*).(jpg|png)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=31536000, s-maxage=31536000, must-revalidate",
-          },
-        ],
-      },
-      {
-        source: "/_next/image(.*)",
-        // has: [
-        //   {
-        //     type: "content-type",
-        //     key: "application/octet-stream",
-        //   },
-        // ],
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=86401, s-maxage=86401, stale-while-revalidate=172801",
-          },
-        ],
-      },
-    ];
-  },
+	reactStrictMode: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		domains: ["www.dropbox.com"],
+		minimumCacheTTL: 86400,
+	},
+	async headers() {
+		return [
+			{
+				source: "/(.*).(jpg|png)",
+				headers: [
+					{
+						key: "Cache-Control",
+						value:
+							"public, max-age=31536000, s-maxage=31536000, must-revalidate",
+					},
+				],
+			},
+			{
+				source: "/_next/image(.*)",
+				// has: [
+				//   {
+				//     type: "content-type",
+				//     key: "application/octet-stream",
+				//   },
+				// ],
+				headers: [
+					{
+						key: "Cache-Control",
+						value:
+							"public, max-age=86401, s-maxage=86401, stale-while-revalidate=172801",
+					},
+				],
+			},
+		];
+	},
 };
