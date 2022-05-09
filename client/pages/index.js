d869e82ec5479e6419c8d741216178312cd1fe4d
thasup <thanachonfirst@hotmail.com>
2022-05-10 03:17:59 +0700

    Fix: hold component rendering until home-page hydration finished

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,7 +4,7 @@ import { Col, Row } from "react-bootstrap";
 import Product from "../components/home/Product";
 
 const Home = ({ products, currentUser }) => {
-	const [onMobile, setOnMobile] = useState(false);
+	const [onMobile, setOnMobile] = useState(null);
 	const [showChild, setShowChild] = useState(false);
 	const [screenWidth, setScreenWidth] = useState(0);
 
@@ -23,26 +23,25 @@ const Home = ({ products, currentUser }) => {
 			setOnMobile(false);
 		}
 
+		if (onMobile !== null) {
+			setShowChild(true);
+		}
+
 		return () => window.removeEventListener("resize", updateSize);
 	}, [screenWidth]);
-
-	// Wait until after client-side hydration to show
-	useEffect(() => {
-		setShowChild(true);
-	}, []);
 
 	return (
 		<>
 			<Row className="mx-0">
 				{products.map((item) => (
 					<Col key={item.id} xs={6} md={4} xl={3} className="p-0">
-						{showChild ? (
+						{showChild && (
 							<Product
 								onMobile={onMobile}
 								product={item}
 								currentUser={currentUser}
 							/>
-						) : null}
+						)}
 					</Col>
 				))}
 			</Row>
