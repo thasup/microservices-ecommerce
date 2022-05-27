a91d3c6a0dadae35fdf2924a546b9c9f54f63b22
thasup <thanachonfirst@hotmail.com>
2022-05-28 00:12:14 +0700

    Refactor: listen for window resize event on top category page

--- client/pages/products/tops/new.js
+++ client/pages/products/tops/new.js
@@ -5,19 +5,29 @@ import Head from "next/head";
 
 import Product from "../../../components/home/Product";
 import Loader from "../../../components/common/Loader";
+import useWindowSize from "../../../hooks/useWindowSize";
 
 const TopsNewArrivals = ({ products, currentUser }) => {
 	const [loading, setLoading] = useState(true);
+	const [onMobile, setOnMobile] = useState(false);
+
+	const { width } = useWindowSize();
 
 	const topsNewArrivals = products
-		.filter((top) => top.category === "Top")
+		?.filter((top) => top.category === "Top")
 		.reverse();
 
 	useEffect(() => {
+		if (width <= 576) {
+			setOnMobile(true);
+		} else {
+			setOnMobile(false);
+		}
+
 		if (products && topsNewArrivals) {
 			setLoading(false);
 		}
-	}, []);
+	}, [width, products]);
 
 	return (
 		<>
@@ -51,7 +61,11 @@ const TopsNewArrivals = ({ products, currentUser }) => {
 					<Row className="mx-0">
 						{topsNewArrivals.map((item) => (
 							<Col key={item.id} xs={6} md={4} xl={3} className="p-0">
-								<Product product={item} currentUser={currentUser} />
+								<Product
+									onMobile={onMobile}
+									product={item}
+									currentUser={currentUser}
+								/>
 							</Col>
 						))}
 					</Row>
