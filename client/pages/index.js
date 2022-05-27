890fc79c3ecf2e4abbe34d23ecf3bbae1424928a
thasup <thanachonfirst@hotmail.com>
2022-05-27 23:44:30 +0700

    Refactor: listen for window resize event on home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,50 +2,35 @@ import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
 import Product from "../components/home/Product";
+import useWindowSize from "../hooks/useWindowSize";
 
 const Home = ({ products, currentUser }) => {
-	const [onMobile, setOnMobile] = useState(null);
-	const [showChild, setShowChild] = useState(false);
-	const [screenWidth, setScreenWidth] = useState(0);
+	const [onMobile, setOnMobile] = useState(false);
+
+	const { width } = useWindowSize();
 
 	useEffect(() => {
-		// Check current window width to determine screen type
-		function updateSize() {
-			setScreenWidth(window.innerWidth);
-		}
+		// setScreenWidth(width);
 
-		window.addEventListener("resize", updateSize);
-		updateSize();
-
-		if (screenWidth <= 576) {
+		if (width <= 576) {
 			setOnMobile(true);
 		} else {
 			setOnMobile(false);
 		}
-
-		if (onMobile !== null) {
-			setShowChild(true);
-		}
-
-		return () => window.removeEventListener("resize", updateSize);
-	}, [screenWidth]);
+	}, [width]);
 
 	return (
-		<>
-			<Row className="mx-0">
-				{products.map((item) => (
-					<Col key={item.id} xs={6} md={4} xl={3} className="p-0">
-						{showChild && (
-							<Product
-								onMobile={onMobile}
-								product={item}
-								currentUser={currentUser}
-							/>
-						)}
-					</Col>
-				))}
-			</Row>
-		</>
+		<Row className="mx-0">
+			{products.map((item) => (
+				<Col key={item.id} xs={6} md={4} xl={3} className="p-0">
+					<Product
+						onMobile={onMobile}
+						product={item}
+						currentUser={currentUser}
+					/>
+				</Col>
+			))}
+		</Row>
 	);
 };
 
