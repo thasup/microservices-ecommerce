09f39a60ef8b2d546f48a0bc87b5e09d0b54ed6b
thasup <thanachonfirst@hotmail.com>
2022-06-04 20:32:30 +0700

    Test: import fonts in app.css

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,9 +3,9 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 import { Container, SSRProvider } from "react-bootstrap";
 
-import "../styles/bootstrap.min.css";
-import "../styles/globals.css";
-// import "../styles/app.css";
+// import "../styles/bootstrap.min.css";
+// import "../styles/globals.css";
+import "../styles/app.css";
 
 import * as ga from "../lib/ga";
 
