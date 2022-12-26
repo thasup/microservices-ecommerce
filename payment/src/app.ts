fa3ead0bc533341c8266251482493c3536b14cbc
thasup <thanachonfirst@hotmail.com>
2022-12-27 02:35:53 +0700

    Set cookie-session to secure

--- payment/src/app.ts
+++ payment/src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
 	cookieSession({
 		signed: false,
-		secure: process.env.NODE_ENV === "production",
+		secure: true,
 	})
 );
 app.use(currentUser);
--- product/src/app.ts
+++ product/src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
 	cookieSession({
 		signed: false,
-		secure: process.env.NODE_ENV === "production",
+		secure: true,
 	})
 );
 app.use(currentUser);
