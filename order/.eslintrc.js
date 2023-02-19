301591f6ae828767da7cbdb3aefa47a47c82c452
thasup <thanachonfirst@hotmail.com>
2023-02-19 23:46:05 +0700

    Add more rule options

--- order/.eslintrc.js
+++ order/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: 'standard-with-typescript',
   overrides: [
   ],
-	parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -15,6 +15,10 @@ module.exports = {
   rules: {
     '@typescript-eslint/dot-notation': 'error',
 		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/prefer-ts-expect-error': 'off'
+		'@typescript-eslint/prefer-ts-expect-error': 'off',
+		'semi': ['error', 'always'],
+    '@typescript-eslint/semi': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-misused-promises': 'off'
   },
 }
