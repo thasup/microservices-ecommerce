6f53d26a111117f46708459c1f734ff7012b8c64
thasup <thanachonfirst@hotmail.com>
2023-03-05 13:08:12 +0700

    Add eslint in user service

---
+++ user/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './user/tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/dot-notation': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/prefer-ts-expect-error': 'off',
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
+  }
+};
