8cf1f84c262e4528840364292808bf0fde88ef19
thasup <thanachonfirst@hotmail.com>
2023-03-13 00:20:55 +0700

    Format product service helper function

--- product/src/app.ts
+++ product/src/app.ts
@@ -1,26 +1,26 @@
-import express from "express";
-import "express-async-errors";
-import cookieSession from "cookie-session";
-import { NotFoundError, errorHandler, currentUser } from "@thasup-dev/common";
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { NotFoundError, errorHandler, currentUser } from '@thasup-dev/common';
 
-import { createProductRouter } from "./routes/create-product";
-import { getProductRouter } from "./routes/get-product";
-import { getAllProductsRouter } from "./routes/get-all-products";
-import { updateProductRouter } from "./routes/update-product";
-import { deleteProductRouter } from "./routes/delete-product";
-import { deleteReviewRouter } from "./routes/delete-review";
-import { createReviewRouter } from "./routes/create-review";
-import { bestsellerRouter } from "./routes/bestseller";
-import { getMyReviewsRouter } from "./routes/get-my-reviews";
+import { createProductRouter } from './routes/create-product';
+import { getProductRouter } from './routes/get-product';
+import { getAllProductsRouter } from './routes/get-all-products';
+import { updateProductRouter } from './routes/update-product';
+import { deleteProductRouter } from './routes/delete-product';
+import { deleteReviewRouter } from './routes/delete-review';
+import { createReviewRouter } from './routes/create-review';
+import { bestsellerRouter } from './routes/bestseller';
+import { getMyReviewsRouter } from './routes/get-my-reviews';
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(
-	cookieSession({
-		signed: false,
-		secure: process.env.NODE_ENV === "production",
-	})
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV === 'production'
+  })
 );
 app.use(currentUser);
 
@@ -34,8 +34,8 @@ app.use(getProductRouter);
 app.use(createReviewRouter);
 app.use(deleteReviewRouter);
 
-app.all("*", async (req, res) => {
-	throw new NotFoundError();
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
