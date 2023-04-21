967671996deacaa3f389d0e59a4ed5fe7ebd1321
thasup <thanachonfirst@hotmail.com>
2023-04-22 00:33:25 +0700

    Fix react/react-in-jsx-scope eslint rule

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document () {
--- client/pages/_error.js
+++ client/pages/_error.js
@@ -1,4 +1,5 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+
 function Error ({ statusCode }) {
   return (
     <p
