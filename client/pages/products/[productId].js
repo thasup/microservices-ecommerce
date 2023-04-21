62b94476f989ff1a259d8f4c13b6738af4e06c5d
thasup <thanachonfirst@hotmail.com>
2023-04-22 02:16:34 +0700

    Format leftover files

--- client/pages/products/[productId].js
+++ client/pages/products/[productId].js
@@ -1,160 +1,161 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, ListGroup, Card, Breadcrumb } from "react-bootstrap";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import Head from "next/head";
+/* eslint-disable multiline-ternary */
+import React, { useState, useEffect } from 'react';
+import { Row, Col, ListGroup, Card, Breadcrumb } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Head from 'next/head';
 
-import Loader from "../../components/common/Loader";
-import Rating from "../../components/common/Rating";
-import NextImage from "../../components/common/NextImage";
-import ProductImageSwiper from "../../components/product/ProductImageSwiper";
-import SocialShare from "../../components/product/SocialShare";
-import ColorSelector from "../../components/common/ColorSelector";
-import SizeSelector from "../../components/common/SizeSelector";
-import ProductDescription from "../../components/product/ProductDescription";
-import Review from "../../components/product/Review";
-import Coupon from "../../components/product/Coupon";
-import AddToCart from "../../components/common/AddToCart";
-import QuantitySelector from "../../components/common/QuantitySelector";
-import YouMayAlsoLike from "../../components/common/YouMayAlsoLike";
-import useWindowSize from "../../hooks/useWindowSize";
+import Loader from '../../components/common/Loader';
+import Rating from '../../components/common/Rating';
+import NextImage from '../../components/common/NextImage';
+import ProductImageSwiper from '../../components/product/ProductImageSwiper';
+import SocialShare from '../../components/product/SocialShare';
+import ColorSelector from '../../components/common/ColorSelector';
+import SizeSelector from '../../components/common/SizeSelector';
+import ProductDescription from '../../components/product/ProductDescription';
+import Review from '../../components/product/Review';
+import Coupon from '../../components/product/Coupon';
+import AddToCart from '../../components/common/AddToCart';
+import QuantitySelector from '../../components/common/QuantitySelector';
+import YouMayAlsoLike from '../../components/common/YouMayAlsoLike';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const productDetail = ({ products, users, currentUser, myOrders }) => {
-	const { productId } = useRouter().query;
+  const { productId } = useRouter().query;
 
-	const [quantity, setQuantity] = useState(1);
-	const [color, setColor] = useState(null);
-	const [size, setSize] = useState(null);
-	const [discountFactor, setDiscountFactor] = useState(1);
+  const [quantity, setQuantity] = useState(1);
+  const [color, setColor] = useState(null);
+  const [size, setSize] = useState(null);
+  const [discountFactor, setDiscountFactor] = useState(1);
 
-	const [initialImage, setInitialImage] = useState(false);
-	const [imageArray, setImageArray] = useState([]);
-	const [imageEvent, setImageEvent] = useState(null);
+  const [initialImage, setInitialImage] = useState(false);
+  const [imageArray, setImageArray] = useState([]);
+  const [imageEvent, setImageEvent] = useState(null);
 
-	const [isPurchase, setIsPurchase] = useState(false);
-	const [onMobile, setOnMobile] = useState(false);
+  const [isPurchase, setIsPurchase] = useState(false);
+  const [onMobile, setOnMobile] = useState(false);
 
-	const [screenWidth, setScreenWidth] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(0);
 
-	const product = products.find((product) => product.id === productId);
-	const categoryParams = `${product?.category.toLowerCase()}${product?.category === "Dress" ? "es" : "s"
+  const product = products.find((product) => product.id === productId);
+  const categoryParams = `${product?.category.toLowerCase()}${product?.category === 'Dress' ? 'es' : 's'
 		}`;
 
-	const { width } = useWindowSize();
+  const { width } = useWindowSize();
 
-	useEffect(() => {
-		setScreenWidth(width);
+  useEffect(() => {
+    setScreenWidth(width);
 
-		if (width <= 576) {
-			setOnMobile(true);
-		} else {
-			setInitialImage(false);
-			setOnMobile(false);
-		}
-	}, [width]);
+    if (width <= 576) {
+      setOnMobile(true);
+    } else {
+      setInitialImage(false);
+      setOnMobile(false);
+    }
+  }, [width]);
 
-	useEffect(async () => {
-		//Check if orders is not an empty array
-		if (myOrders && myOrders.length !== 0) {
-			// Check if user can write a review after purchased the product
-			const hasPurchasedItem = await myOrders.map((order) => {
-				if (order.isPaid === true) {
-					return order.cart.some((item) => item.productId === productId);
-				}
-				return false;
-			});
+  useEffect(async () => {
+    // Check if orders is not an empty array
+    if (myOrders && myOrders.length !== 0) {
+      // Check if user can write a review after purchased the product
+      const hasPurchasedItem = await myOrders.map((order) => {
+        if (order.isPaid === true) {
+          return order.cart.some((item) => item.productId === productId);
+        }
+        return false;
+      });
 
-			// If some order contains the purchased product set isPurchase to true
-			if (hasPurchasedItem.includes(true)) {
-				setIsPurchase(true);
-			}
-		}
+      // If some order contains the purchased product set isPurchase to true
+      if (hasPurchasedItem.includes(true)) {
+        setIsPurchase(true);
+      }
+    }
 
-		// Defined variable
-		const mainImage = document.getElementsByClassName("product-main-img");
-		const sideImage = document.getElementsByClassName("product-side-img");
+    // Defined variable
+    const mainImage = document.getElementsByClassName('product-main-img');
+    const sideImage = document.getElementsByClassName('product-side-img');
 
-		// Toggle the first image to show as a main image
-		// when page load at first time on desktop screen
-		if (!initialImage && !onMobile) {
-			for (let i = 0; i < mainImage.length; i++) {
-				mainImage[i].classList.remove("toggle-main-img");
-				sideImage[i].classList.remove("toggle-side-img");
-			}
+    // Toggle the first image to show as a main image
+    // when page load at first time on desktop screen
+    if (!initialImage && !onMobile) {
+      for (let i = 0; i < mainImage.length; i++) {
+        mainImage[i].classList.remove('toggle-main-img');
+        sideImage[i].classList.remove('toggle-side-img');
+      }
 
-			mainImage[0].classList.add("toggle-main-img");
-			setInitialImage(true);
-		}
+      mainImage[0].classList.add('toggle-main-img');
+      setInitialImage(true);
+    }
 
-		// Toggle 'toggle-main-img' class for image when user clicked on that side image
-		if (imageEvent) {
-			for (let i = 0; i < mainImage.length; i++) {
-				mainImage[i].classList.remove("toggle-main-img");
-				sideImage[i].classList.remove("toggle-side-img");
-			}
+    // Toggle 'toggle-main-img' class for image when user clicked on that side image
+    if (imageEvent) {
+      for (let i = 0; i < mainImage.length; i++) {
+        mainImage[i].classList.remove('toggle-main-img');
+        sideImage[i].classList.remove('toggle-side-img');
+      }
 
-			const currentId =
+      const currentId =
 				imageEvent.target.parentElement.parentElement.id.slice(-1);
 
-			mainImage[currentId].classList.add("toggle-main-img");
+      mainImage[currentId].classList.add('toggle-main-img');
 
-			imageEvent.target.parentElement.parentElement.classList.add(
-				"toggle-side-img"
-			);
+      imageEvent.target.parentElement.parentElement.classList.add(
+        'toggle-side-img'
+      );
 
-			// Set image event to default
-			setImageEvent(null);
-		}
+      // Set image event to default
+      setImageEvent(null);
+    }
 
-		// Limit quantity input by locked maximum and minimum from the product countInStock
-		if (quantity > product.countInStock) {
-			setQuantity(product.countInStock);
-		} else if (quantity < 1) {
-			setQuantity(1);
-		}
-	}, [product, initialImage, imageEvent, quantity]);
+    // Limit quantity input by locked maximum and minimum from the product countInStock
+    if (quantity > product.countInStock) {
+      setQuantity(product.countInStock);
+    } else if (quantity < 1) {
+      setQuantity(1);
+    }
+  }, [product, initialImage, imageEvent, quantity]);
 
-	if (imageArray.length === 0 && product) {
-		const filterImages = Object.values(product.images).filter(
-			(image) => image !== null && image !== ""
-		);
+  if (imageArray.length === 0 && product) {
+    const filterImages = Object.values(product.images).filter(
+      (image) => image !== null && image !== ''
+    );
 
-		setImageArray(filterImages);
-	}
+    setImageArray(filterImages);
+  }
 
-	useEffect(() => {
-		// Re-evaluate new filter images when the product had changed
-		if (product) {
-			const filterImages = Object.values(product?.images).filter(
-				(image) => image !== null && image !== ""
-			);
+  useEffect(() => {
+    // Re-evaluate new filter images when the product had changed
+    if (product) {
+      const filterImages = Object.values(product?.images).filter(
+        (image) => image !== null && image !== ''
+      );
 
-			setImageArray(filterImages);
-			if (initialImage) {
-				setInitialImage(false);
-			}
-		}
-	}, [product]);
+      setImageArray(filterImages);
+      if (initialImage) {
+        setInitialImage(false);
+      }
+    }
+  }, [product]);
 
-	const colorSelectedHandler = (color) => {
-		if (color !== null) {
-			setColor(color);
-		}
-	};
+  const colorSelectedHandler = (color) => {
+    if (color !== null) {
+      setColor(color);
+    }
+  };
 
-	const sizeSelectedHandler = (size) => {
-		if (size !== null) {
-			setSize(size);
-		}
-	};
+  const sizeSelectedHandler = (size) => {
+    if (size !== null) {
+      setSize(size);
+    }
+  };
 
-	const couponHandler = (factor) => {
-		if (factor) {
-			setDiscountFactor(factor);
-		}
-	};
+  const couponHandler = (factor) => {
+    if (factor) {
+      setDiscountFactor(factor);
+    }
+  };
 
-	return (
+  return (
 		<>
 			<Head>
 				<title>{product.title} | Aurapan</title>
@@ -163,7 +164,7 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 				{!product.id || product.id !== productId ? (
 					<div
 						className="d-flex justify-content-center align-items-center px-0"
-						style={{ marginTop: "80px" }}
+						style={{ marginTop: '80px' }}
 					>
 						<Loader />
 					</div>
@@ -188,11 +189,13 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 						</Breadcrumb>
 
 						<Row id="product-page">
-							{onMobile ? (
+							{onMobile
+							  ? (
 								<Col className="mb-3">
 									<ProductImageSwiper product={product} />
 								</Col>
-							) : (
+							    )
+							  : (
 								<>
 									<Col sm={1} className="mb-3">
 										{imageArray.map((img, index) => (
@@ -225,7 +228,7 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 										))}
 									</Col>
 								</>
-							)}
+							    )}
 
 							<Col sm={6}>
 								<ListGroup variant="flush" className="mb-3">
@@ -247,9 +250,9 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 											<ColorSelector
 												product={product}
 												callback={colorSelectedHandler}
-												margin={"5px"}
-												size={"2rem"}
-												flex={"start"}
+												margin={'5px'}
+												size={'2rem'}
+												flex={'start'}
 											/>
 										</div>
 									</ListGroup.Item>
@@ -259,7 +262,7 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 										<div className="my-1 px-0">
 											<SizeSelector
 												product={product}
-												width={"35px"}
+												width={'35px'}
 												callback={sizeSelectedHandler}
 											/>
 										</div>
@@ -294,9 +297,8 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 												</Col>
 												<Col>
 													<h6>
-														{product.countInStock > 0
-															? "In Stock"
-															: "Out of Stock"}
+														  ? 'In Stock'
+														  : 'Out of Stock'}
 													</h6>
 												</Col>
 											</Row>
@@ -354,7 +356,7 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 												size={size}
 												quantity={quantity}
 												discountFactor={discountFactor}
-												lg={onMobile ? true : false}
+												lg={!!onMobile}
 											/>
 										</ListGroup.Item>
 									</ListGroup>
@@ -395,7 +397,7 @@ const productDetail = ({ products, users, currentUser, myOrders }) => {
 				)}
 			</div>
 		</>
-	);
+  );
 };
 
 export default productDetail;
