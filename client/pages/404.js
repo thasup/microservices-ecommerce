a8c8f96ec169b1cc9eca6fa130619d4754379845
Thanachon <thanachonfirst@hotmail.com>
2024-12-07 23:33:44 +0700

    fix: Remove getInitialProps from custom error page

--- client/pages/404.js
+++ client/pages/404.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
-function Custom404({ statusCode }) {
+function Custom404() {
 	return (
 		<p
 			className="d-flex justify-content-center align-items-center"
 			style={{ height: '90vh' }}
 		>
-			{
-				statusCode === 404
-					? '404 Page not found X('
-					: 'Something went wrong. Please try again. :('
-			}
+			404 Page not found :(
 		</p>
 	);
 }
-
-Custom404.getInitialProps = ({ res, err }) => {
-	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-	return { statusCode };
-};
 
 export default Custom404
